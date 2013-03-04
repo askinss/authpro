92d20d6869388bdf5ef6fe63310391928143b938
Richard Nyström <ricny046@gmail.com>
2013-03-04 23:49:22 +0100

    Cleaned up test

--- test/authpro_generator_test.rb
+++ test/authpro_generator_test.rb
@@ -3,14 +3,13 @@ require 'rails'
 
 class AuthproGeneratorTest < Rails::Generators::TestCase
   tests AuthproGenerator
-  destination File.expand_path("../tmp/dummy", File.dirname(__FILE__))
+  destination File.expand_path("../tmp", File.dirname(__FILE__))
   setup :prepare_destination
 
   test "Assert all files are properly created" do
     run_generator
-    FileUtils.cp_r "dummy", "../tmp"
+    #FileUtils.cp_r "dummy/*", "../tmp"
     assert_file "config/initializers/devise.rb"
     assert_file "config/locales/devise.en.yml"
   end
-
 end
