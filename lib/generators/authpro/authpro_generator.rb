acb034d9dd8747552dea5d7e1cce7b19084bb465
Richard Nyström <ricny046@gmail.com>
2013-03-09 22:38:26 +0100

    More passing tests.

--- lib/generators/authpro/authpro_generator.rb
+++ lib/generators/authpro/authpro_generator.rb
@@ -2,47 +2,44 @@ class AuthproGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
   def copy_models
-    puts "copy_models"
     copy_file "user.rb", "app/models/user.rb"
   end
 
-  # def copy_controllers
-  #   puts "copy_controllers"
-  #   copy_file "users_controller.rb", "app/controllers/users_controller.rb"
-  #   copy_file "sessions_controller.rb", "app/controllers/sessions_controller.rb"
-  #   copy_file "password_resets_controller.rb", "app/controllers/password_resets_controller.rb"
-  #   copy_file "home_controller.rb", "app/controllers/home_controller.rb"
+  def copy_controllers
+    copy_file "users_controller.rb", "app/controllers/users_controller.rb"
+    copy_file "sessions_controller.rb", "app/controllers/sessions_controller.rb"
+    copy_file "password_resets_controller.rb", "app/controllers/password_resets_controller.rb"
+    copy_file "home_controller.rb", "app/controllers/home_controller.rb"
     
-  #   inject_into_file "app/controllers/application_controller.rb", :after => "protect_from_forgery with: :exception\n" do
-  #     "   private\n\n" +
-  #     "   def current_user\n" +
-  #     "     @current_user ||= User.find_by_auth_token( cookies[:auth_token]) if cookies[:auth_token]\n" +
-  #     "   end\n\n" +
-  #     "   helper_method :current_user\n"
-  #   end
-  # end
+    inject_into_file "app/controllers/application_controller.rb", :after => "protect_from_forgery with: :exception\n" do
+      "\n" +
+      "  private\n\n" +
+      "  def current_user\n" +
+      "    @current_user ||= User.find_by_auth_token( cookies[:auth_token]) if cookies[:auth_token]\n" +
+      "  end\n\n" +
+      "  helper_method :current_user\n"
+    end
+  end
 
-  # def copy_views
-  #   puts "copy_views"
-  #   copy_file "new_user.html.erb", "app/views/users/new.html.erb"
-  #   copy_file "new_password_resets.html.erb", "app/views/password_resets/new.html.erb"
-  #   copy_file "password_resets_edit.html.erb", "app/views/password_resets/edit.html.erb"
-  #   copy_file "new_sessions.html.erb", "app/views/sessions/new.html.erb"
-  #   copy_file "application.html.erb", "app/views/layouts/application.html.erb"
-  #   copy_file "index.html.erb", "app/views/home/index.html.erb"
-  #   copy_file "password_reset.text.erb", "app/views/user_mailer/password_reset.text.erb"
-  # end
+  def copy_views
+    copy_file "new_user.html.erb", "app/views/users/new.html.erb"
+    copy_file "new_password_resets.html.erb", "app/views/password_resets/new.html.erb"
+    copy_file "password_resets_edit.html.erb", "app/views/password_resets/edit.html.erb"
+    copy_file "new_sessions.html.erb", "app/views/sessions/new.html.erb"
+    copy_file "application.html.erb", "app/views/layouts/application.html.erb"
+    copy_file "index.html.erb", "app/views/home/index.html.erb"
+    copy_file "password_reset.text.erb", "app/views/user_mailer/password_reset.text.erb"
+  end
 
-  # def add_routes
-  #   puts "add_routes"
-  #   route "resources :password_resets"
-  #   route "resources :sessions"
-  #   route "resources :users"
-  #   route "get 'login' => 'sessions#new', :as => 'login'"
-  #   route "get 'signup' => 'users#new', :as => 'signup'"
-  #   route "get 'logout' => 'sessions#destroy', :as => 'logout'"
-  #   route "root to: 'home#index'"
-  # end
+  def add_routes
+    route "resources :password_resets"
+    route "resources :sessions"
+    route "resources :users"
+    route "get 'login' => 'sessions#new', :as => 'login'"
+    route "get 'signup' => 'users#new', :as => 'signup'"
+    route "get 'logout' => 'sessions#destroy', :as => 'logout'"
+    route "root to: 'home#index'"
+  end
 
   # def copy_mailers
   #   puts "copy_mailers"
--- test/authpro_generator_test.rb
+++ test/authpro_generator_test.rb
@@ -8,7 +8,27 @@ class AuthproGeneratorTest < Rails::Generators::TestCase
   #teardown :cleanup_destination_root
   
   test "Assert all files are properly created" do 
-    run_generator
+    run_generator(["--force"])
+    # models
     assert_file "app/models/user.rb"
+    
+    # controllers
+    assert_file "app/controllers/users_controller.rb"
+    assert_file "app/controllers/sessions_controller.rb"
+    assert_file "app/controllers/password_resets_controller.rb"
+    assert_file "app/controllers/home_controller.rb"
+
+    # views
+    assert_file "app/views/users/new.html.erb"
+    assert_file "app/views/password_resets/new.html.erb"
+    assert_file "app/views/password_resets/edit.html.erb"
+    assert_file "app/views/sessions/new.html.erb"
+    assert_file "app/views/layouts/application.html.erb"
+    assert_file "app/views/home/index.html.erb"
+    assert_file "app/views/user_mailer/password_reset.text.erb"
+  
   end
+
+  # todo: test application_controller injection.
+  # todo : test routes
 end
