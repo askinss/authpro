49c1586cdb68a60fa5e93f32abaa0de0d3f54ee2
Richard Nyström <ricny046@gmail.com>
2013-03-09 21:18:35 +0100

    copying folders i Ruby suckz...

--- test/authpro_generator_test.rb
+++ test/authpro_generator_test.rb
@@ -3,12 +3,11 @@ require 'rails'
 
 class AuthproGeneratorTest < Rails::Generators::TestCase
   tests AuthproGenerator
-  destination File.expand_path("../tmp", File.dirname(__FILE__))
-  setup :prepare_destination
+  destination File.expand_path("rails/dummy", File.dirname(__FILE__))
+  #setup :prepare_destination
 
   test "Assert all files are properly created" do
     run_generator
-    #FileUtils.cp_r "dummy/*", "../tmp"
     assert_file "config/initializers/devise.rb"
     assert_file "config/locales/devise.en.yml"
   end
