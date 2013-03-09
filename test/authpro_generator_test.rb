dc3b6430eeaa418d424ec218e2df619e640230ff
Richard Nyström <ricny046@gmail.com>
2013-03-09 22:26:47 +0100

    First test passing.

--- test/authpro_generator_test.rb
+++ test/authpro_generator_test.rb
@@ -3,12 +3,12 @@ require 'rails'
 
 class AuthproGeneratorTest < Rails::Generators::TestCase
   tests AuthproGenerator
-  destination File.expand_path("rails/dummy", File.dirname(__FILE__))
+  destination __dir__ + "/rails/dummy"
   #setup :prepare_destination
-
-  test "Assert all files are properly created" do
+  #teardown :cleanup_destination_root
+  
+  test "Assert all files are properly created" do 
     run_generator
-    assert_file "config/initializers/devise.rb"
-    assert_file "config/locales/devise.en.yml"
+    assert_file "app/models/user.rb"
   end
 end
