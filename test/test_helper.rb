b0f132a8398cfef75e8e322e72d5bf85021e1fd7
Richard Nyström <ricny046@gmail.com>
2013-03-10 03:03:55 +0100

    Cleaned up test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,6 @@
 ENV["RAILS_ENV"] = "test"
 
 FileUtils.cp_r __dir__ + "/dummy", __dir__ + "/rails"
-#require File.expand_path('../rails/dummy/config/application.rb', __FILE__)
 require File.expand_path("../rails/dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
 
