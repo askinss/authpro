37f866dc6a50be0d2facf450247c408ff03c42ce
Richard Nyström <ricny046@gmail.com>
2013-03-10 02:30:47 +0100

    Cleaned up the test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,38 +2,28 @@
 ENV["RAILS_ENV"] = "test"
 
 FileUtils.cp_r __dir__ + "/dummy", __dir__ + "/rails"
+#require File.expand_path('../rails/dummy/config/application.rb', __FILE__)
 require File.expand_path("../rails/dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
+
 require 'capybara/rails'
 require "database_cleaner"
 Rails.backtrace_cleaner.remove_silencers!
 
-# Load support files
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-
-# Load fixtures from the engine
-if ActiveSupport::TestCase.method_defined?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
-end
-
 require "rails/generators/test_case"
 require "generators/authpro/authpro_generator"
 
-# Transactional fixtures do not work with Selenium tests, because Capybara
-# uses a separate server thread, which the transactions would be hidden
-# from. We hence use DatabaseCleaner to truncate our test database.
 DatabaseCleaner.strategy = :truncation
 
 class ActionDispatch::IntegrationTest
-  # Make the Capybara DSL available in all integration tests
   include Capybara::DSL
 
-  # Stop ActiveRecord from wrapping tests in transactions
   self.use_transactional_fixtures = false
 
   teardown do
-    DatabaseCleaner.clean       # Truncate the database
-    Capybara.reset_sessions!    # Forget the (simulated) browser state
-    Capybara.use_default_driver # Revert Capybara.current_driver to Capybara.default_driver
+    DatabaseCleaner.clean
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
   end
 end
+
