ba44e51373010f2528d22ae59a3a950f2c63de28
Richard Nyström <ricny046@gmail.com>
2013-03-28 09:52:39 +0100

    Version bump

--- test/rails/dummy/app/models/user.rb
+++ test/rails/dummy/app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
   
-  validates :password, presence: true, on: :create
   validates :email, presence: true, uniqueness: true, format: /@/
   
   before_create { generate_token(:auth_token) }
